f0165c413651a3bf813a25498751573773a69465
cgrif27 <62336500+cgrif27@users.noreply.github.com>
2021-08-14 17:16:56 +1000

    First

---
+++ MatrixMultiply/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace MatrixMultiply
+{
+    class Program
+    {
+        static double[][] MatrixCreate(int rows, int cols)
+        {
+            // do error checking here
+            double[][] result = new double[rows][];
+            for (int i = 0; i < rows; ++i)
+                result[i] = new double[cols];
+            return result;
+        }
+
+        static double[][] MatrixProductSeq(double[][] matrixA, double[][] matrixB)
+        {
+            int aRows = matrixA.Length; int aCols = matrixA[0].Length;
+            int bRows = matrixB.Length; int bCols = matrixB[0].Length;
+            if (aCols != bRows)
+                throw new Exception("xxx");
+
+            double[][] result = MatrixCreate(aRows, bCols);
+
+            for (int i = 0; i < aRows; ++i) // each row of A
+                for (int j = 0; j < bCols; ++j) // each col of B
+                    for (int k = 0; k < aCols; ++k) // could use k < bRows
+                        result[i][j] += matrixA[i][k] * matrixB[k][j];
+            return result;
+        }
+
+        static double[][] MatrixProduct(double[][] matrixA, double[][] matrixB)
+        {
+            int aRows = matrixA.Length; int aCols = matrixA[0].Length;
+            int bRows = matrixB.Length; int bCols = matrixB[0].Length;
+            if (aCols != bRows)
+                throw new Exception("xxxx");
+
+            double[][] result = MatrixCreate(aRows, bCols);
+
+            Parallel.For(0, aRows, i =>
+            {
+                for (int j = 0; j < bCols; ++j) // each col of B
+                    for (int k = 0; k < aCols; ++k) // could use k < bRows
+                        result[i][j] += matrixA[i][k] * matrixB[k][j];
+            }
+            );
+
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            global::System.Console.WriteLine("sdsdsd");
+
+        }
+    }
+}
